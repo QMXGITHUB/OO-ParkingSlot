830d13df6759da710886e1c739f2901901028ee8
mxqiao <mxqiao@thoughtworks.com>
2015-11-14 13:16:55 +0800

    [qmx] init ParkingSlot project

---
+++ OOParkingslot/ParkingSlotTest.cs
@@ -0,0 +1,14 @@
+﻿using Xunit;
+
+namespace OOParkingslot
+{
+    public class ParkingSlotTest
+    {
+        [Fact]
+        public void When_Hao_parked_a_car_in_Parkingslot_he_can_take_the_car_away()
+        {
+            Assert.Equal(true,false);
+        }
+
+    }
+}
