00d7fd5eb41d4197b371dd8f8f2a6fb8270e3aa7
mxqiao <mxqiao@thoughtworks.com>
2015-11-25 22:36:20 +0800

    [qmx] reset parkingboy

--- OOParkingslot/ParkingBoy.cs
+++ OOParkingslot/ParkingBoy.cs
@@ -4,17 +4,6 @@ namespace OOParkingslot
 {
     public class ParkingBoy
     {
-        private Parkinglot parkinglot;
-
-        public ParkingBoy(Parkinglot parkinglot)
-        {
-            this.parkinglot = parkinglot;
-        }
-
-        public Car Pick(string parkingToken)
-        {
-            return parkinglot.Pick(parkingToken);
-        }
 
     }
 }
--- OOParkingslot/ParkingBoyTest.cs
+++ OOParkingslot/ParkingBoyTest.cs
@@ -4,17 +4,5 @@ namespace OOParkingslot
 {
     public class ParkingBoyTest
     {
-        [Fact]
-        public void should_pickup_car()
-        {
-            var parkinglot = new Parkinglot();
-            var parkingBoy = new ParkingBoy(parkinglot);
-
-            var car = new Car();
-            var parkingToken = parkinglot.Park(car);
-
-            Assert.Same(car, parkingBoy.Pick(parkingToken) );
-        }
-
     }
 }
