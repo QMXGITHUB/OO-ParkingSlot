e748e2a66047c9b763dc5d92c94be5251311a948
mxqiao <mxqiao@thoughtworks.com>
2015-11-25 22:40:51 +0800

    [qmx] parkboy can pickup car when there is only one parkinglot

--- OOParkingslot/ParkingBoy.cs
+++ OOParkingslot/ParkingBoy.cs
@@ -1,9 +1,17 @@
-﻿using System.Linq;
-
-namespace OOParkingslot
+﻿namespace OOParkingslot
 {
     public class ParkingBoy
     {
+        private Parkinglot parkinglot;
 
+        public ParkingBoy(Parkinglot parkinglot)
+        {
+            this.parkinglot = parkinglot;
+        }
+
+        public Car Pick(string parkToken)
+        {
+            return parkinglot.Pick(parkToken);
+        }
     }
 }
--- OOParkingslot/ParkingBoyTest.cs
+++ OOParkingslot/ParkingBoyTest.cs
@@ -4,5 +4,17 @@ namespace OOParkingslot
 {
     public class ParkingBoyTest
     {
+        [Fact]
+        public void should_pick_car_when_only_one_parkinglot_parked()
+        {
+            var parkinglot = new Parkinglot();
+            var parkingBoy = new ParkingBoy(parkinglot);
+
+            var car = new Car();
+            var parkToken = parkinglot.Park(car);
+
+            Assert.Same(car, parkingBoy.Pick(parkToken));
+        }
+
     }
 }
