7758a68475b7371b437384dfb736c4fcf5e239f8
mxqiao <mxqiao@thoughtworks.com>
2015-11-25 22:09:24 +0800

    [qmx] add test parkingboy can pick car when parkinglot parked

--- OOParkingslot/ParkingBoy.cs
+++ OOParkingslot/ParkingBoy.cs
@@ -4,28 +4,17 @@ namespace OOParkingslot
 {
     public class ParkingBoy
     {
-        private Parkinglot[] parkinglots;
+        private Parkinglot parkinglot;
 
-        public ParkingBoy(Parkinglot[] parkinglots)
+        public ParkingBoy(Parkinglot parkinglot)
         {
-            this.parkinglots = parkinglots;
+            this.parkinglot = parkinglot;
         }
 
-        public string Parking(Car car)
+        public Car Pick(string parkingToken)
         {
-            var parkinglot = parkinglots.FirstOrDefault(parkingLot => !parkingLot.IsFull());
-            if (parkinglot == null) return null;
-            return parkinglot.Park(car);
+            return parkinglot.Pick(parkingToken);
         }
 
-        public Car Pickupwith(string parkingToken)
-        {
-            Car car = null;
-            foreach (var parkinglot in parkinglots)
-            {
-                if ((car = parkinglot.Pick(parkingToken)) != null) break;
-            }
-            return car;
-        }
     }
 }
--- OOParkingslot/ParkingBoyTest.cs
+++ OOParkingslot/ParkingBoyTest.cs
@@ -5,60 +5,16 @@ namespace OOParkingslot
     public class ParkingBoyTest
     {
         [Fact]
-        public void should_pick_up_car_when_parkingboy_parked()
+        public void should_pickup_car()
         {
-            var parkingBoy = new ParkingBoy(new Parkinglot[]{new Parkinglot()});
-            
-            var parkingToken = parkingBoy.Parking(new Car());
+            var parkinglot = new Parkinglot();
+            var parkingBoy = new ParkingBoy(parkinglot);
 
-            Assert.NotNull(parkingBoy.Pickupwith(parkingToken));
+            var car = new Car();
+            var parkingToken = parkinglot.Park(car);
+
+            Assert.Same(car, parkingBoy.Pick(parkingToken) );
         }
-
-        [Fact]
-        public void should_success_when_parkinglota_is_not_full_and_parkinglotB_is_full()
-        {
-            var parkingBoy = new ParkingBoy(new Parkinglot[2] { new Parkinglot(1), new Parkinglot(0) });
-            
-            var parkingToken = parkingBoy.Parking(new Car());
-
-            Assert.NotNull(parkingToken);
-        }
-
-        [Fact]
-        public void should_success_when_parkinglotA_is_full_and_parkinglotB_is_not_full()
-        {
-            var parkingBoy = new ParkingBoy(new Parkinglot[2] {new Parkinglot(0), new Parkinglot(2)});
-            parkingBoy.Parking(new Car());
-            
-            var parkingToken = parkingBoy.Parking(new Car());
-
-            Assert.NotNull(parkingToken);
-        }
-
-        [Fact]
-        public void should_failed_when_parkinglotA_and_parkinglotB_are_all_full()
-        {
-            var parkingBoy = new ParkingBoy(new Parkinglot[2] { new Parkinglot(1), new Parkinglot(1) });
-            parkingBoy.Parking(new Car());
-            parkingBoy.Parking(new Car());
-
-            var parkingToken = parkingBoy.Parking(new Car());
-
-            Assert.Null(parkingToken);
-        }
-
-        [Fact]
-        public void should_pick_up_success_when_car_parked_in_parkinglotB()
-        {
-            var parkingBoy = new ParkingBoy(new Parkinglot[2] { new Parkinglot(0), new Parkinglot(1) });
-            var qq = new Car();
-            var parkingTokenQq = parkingBoy.Parking(qq);
-
-            var pickupQq = parkingBoy.Pickupwith(parkingTokenQq);
-
-            Assert.Same(qq, pickupQq);
-        }
-
 
     }
 }
