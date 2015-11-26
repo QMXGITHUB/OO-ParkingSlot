6f55288b441fe351cba0c66afc7781fab51e5164
mxqiao <mxqiao@thoughtworks.com>
2015-11-26 22:35:00 +0800

    [qmx] refactor remove useless references

--- OOParkingslot/SuperParkingBoy.cs
+++ OOParkingslot/SuperParkingBoy.cs
@@ -1,11 +1,8 @@
-﻿using System;
-using System.Collections.Generic;
-using System.Linq;
-using System.Text;
+﻿using System.Linq;
 
 namespace OOParkingslot
 {
-    class SuperParkingBoy
+    public class SuperParkingBoy
     {
         private Parkinglot[] parkinglots;
         readonly SmartParkingBoy smartParkingBoy;
