086d437b85c59d66f3a87b1f7be649f388d93599
mxqiao <mxqiao@thoughtworks.com>
2015-12-06 13:34:42 +0800

    [qmx] move all implement of iParkingboy into one file

--- OOParkingslot/IParkingPolicy.cs
+++ OOParkingslot/IParkingPolicy.cs
@@ -1,5 +1,32 @@
-﻿namespace OOParkingslot
+﻿using System.Linq;
+
+namespace OOParkingslot
 {
+    public class MoreAvaibleStallsParking : IParkingPolicy
+    {
+        public Parkinglot FindParkinglotToPark(Parkinglot[] parkinglots)
+        {
+            return parkinglots.OrderByDescending(parkinglot => parkinglot.GetAvailableStallsCount())
+                .First();
+        }
+    }
+
+    public class HigherVacancyRateParking : IParkingPolicy
+    {
+        public Parkinglot FindParkinglotToPark(Parkinglot[] parkinglots)
+        {
+            return parkinglots.OrderByDescending(parkinglot => parkinglot.GetVacancyRate()).First();
+        }
+    }
+
+    public class SequentParking: IParkingPolicy
+    {
+        public Parkinglot FindParkinglotToPark(Parkinglot[] parkinglots)
+        {
+            return parkinglots.FirstOrDefault(parkinglot => parkinglot.IsFull() == false);
+        }
+    }
+
     public interface IParkingPolicy
     {
         Parkinglot FindParkinglotToPark(Parkinglot[] parkinglots);
